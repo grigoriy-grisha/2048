2b45d58b0609a5f3fccdd41e0ccbe3786db6b87b
Gavrilov Grigoriy <g.gavrilov@worksolutions.ru>
2022-07-14 02:09:22 +0300

    feat. Реализованы свайпы во все стороны в модели

--- src/index.tsx
+++ src/index.tsx
@@ -1,39 +1,29 @@
-import type { Component } from 'solid-js';
-import { render } from 'solid-js/web'
+import type { Component } from "solid-js";
+import { render } from "solid-js/web";
 import { createEvent, createStore } from "effector";
-import { useUnit } from 'effector-solid'
+import { useUnit } from "effector-solid";
+import { moveBottomEvent } from "./model/fieldsModel";
 
-const inputText = createEvent<string>()
+const inputText = createEvent<string>();
 
-const $text = createStore('')
-const $size = createStore(0)
+const $text = createStore("");
+const $size = createStore(0);
 
-$text.on(inputText, (_, text) => text)
-$size.on(inputText, (_, text) => text.length)
+$text.on(inputText, (_, text) => text);
+$size.on(inputText, (_, text) => text.length);
 
 const Form = () => {
   const { text, size, inputTextEvent } = useUnit({
     size: $size,
     text: $text,
-    inputTextEvent: inputText
-  })
+    inputTextEvent: inputText,
+  });
 
-  return (
-    <form>
-      <input
-        type="text"
-        onInput={e => inputTextEvent(e.currentTarget.value)}
-        value={text()}
-      />
-      <p>Length: {size}</p>
-    </form>
-  )
-}
-
-const App: Component = () => {
-  return (
-    <Form />
-  );
+  return <div onClick={moveBottomEvent as any}>click</div>;
 };
 
-render(() => <App />, document.getElementById('root')!)
+const App: Component = () => {
+  return <Form />;
+};
+
+render(() => <App />, document.getElementById("root")!);
